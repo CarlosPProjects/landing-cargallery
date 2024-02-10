1dc96925d636e028a138ca7348df7022fc4af6e9
CarlosPProjects <cg9822@gmail.com>
2024-02-10 14:07:20 +0100

    initial state modified

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,31 @@
-import type { Config } from "tailwindcss";
+import {fontFamily} from "tailwindcss/defaultTheme"
 
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container:  {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1440px",
+      }
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: "hsl(var(--primary))",
+        secondary: "(var(--secondary))",
+        body: "(var(--body))",
+        accent: "(var(--accent))", 
+      },
+      fontFamily: {
+        sans: ["var(--font-sans)", ...fontFamily.sans],
       },
     },
   },
   plugins: [],
 };
-export default config;
