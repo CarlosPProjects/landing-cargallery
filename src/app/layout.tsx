0503f5d89dedd342487efe32a759cce977692109
CarlosPProjects <cg9822@gmail.com>
2024-02-10 20:57:35 +0100

    Header setted, base web structure configured

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import { cn } from "@/lib/utils";
 import "@/styles/globals.css";
+import Header from "@/components/Header";
 
 export const fontSans = FontSans({
   subsets: ["latin"],
@@ -20,8 +21,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={cn("font-sans antialiased", fontSans.variable)}>
-        <main>
-          {children}
+        <main className="container min-h-screen flex flex-col gap-4">
+          <Header />
+          <section className="flex-1 border">
+            {children}
+          </section>
         </main>
       </body>
     </html>
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className="py-4">
+            <p className="text-primary font-bold text-lg tracking-tight text-center">collectcar.</p>
+        </header>
+    )
+}
+
+export default Header
